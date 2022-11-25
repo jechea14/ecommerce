4e7300605a6ccc42aec223fce834bf4a75aadbeb
Jeanie Chea <chea.jeanie@gmail.com>
2022-11-24 21:33:28 -0800

    Start with Nextjs, Typescript, Tailwindcss

---
+++ sample/pages/index.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1 className='text-3xl font-bold underline'>Hello Next.js 👋</h1>
+    <p>
+      <Link href="/about">About</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
