0afe9df6434982da771de2b30a6a7412a8b5f3fe
Jeanie Chea <chea.jeanie@gmail.com>
2022-12-03 01:35:00 -0800

    Install Mantine UI and configure with nextjs and tailwindcss

--- ecommerce/pages/index.tsx
+++ ecommerce/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Button } from '@mantine/core';
 
 export default function Home() {
   return (
@@ -15,6 +16,10 @@ export default function Home() {
         <h1 className='text-3xl font-bold underline'>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Button color="yellow" radius="xl" size="lg">
+          Settings
+        </Button>
 
         <p className={styles.description}>
           Get started by editing{' '}
