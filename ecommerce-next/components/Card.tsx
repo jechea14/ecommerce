65246d5e7f91bfc33e55d61548a697f02ccb5499
Jeanie Chea <chea.jeanie@gmail.com>
2022-12-25 16:23:36 -0800

    Resolve Image size

--- ecommerce-next/components/Card.tsx
+++ ecommerce-next/components/Card.tsx
@@ -9,7 +9,13 @@ type CardProps = {
 export const Card: React.FC<CardProps> = ({item}) => {
     return (
         <div className='border rounded-lg p-4 bg-white'>
-            <Image src={item.image[0]} alt={item.name} className='rounded-tl-lg rounded-tr-lg'/>
+            <Image 
+                src={item.image[0]} 
+                alt={item.name}  
+                width={500}
+                height={500}
+                className='rounded-tl-lg rounded-tr-lg'
+            />
             <div className='flex flex-col justify-between'>
                 <h1>{item.name}</h1>
                 <strong className='text-lg'>${item.price.toFixed(2)}</strong>
