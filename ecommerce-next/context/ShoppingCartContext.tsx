144ce5738f8bb51a845c9c3813085416f4edd8a3
Jeanie Chea <chea.jeanie@gmail.com>
2023-01-14 23:41:10 -0800

    Update ShoppingCartContext.tsx

--- ecommerce-next/context/ShoppingCartContext.tsx
+++ ecommerce-next/context/ShoppingCartContext.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { createContext, ReactNode, useContext, useState } from 'react'
+import { Product } from "../interfaces/index"
 
-const ShoppingCartContext = () => {
-  return (
-    <div>ShoppingCartContext</div>
-  )
+type ShoppingCartProviderProps = {
+  children: ReactNode
 }
 
-export default ShoppingCartContext
+type ShoppingCartContext = {
+  addToCart: (item: Product, amount: number) => void
+  cartItems: CartItem[]
+}
+
+type CartItem = {
+  id: number,
+  quantity: number
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
+
+export const useShoppingCart = () => useContext(ShoppingCartContext)
+
+export const ShoppingCartProvider = ({children}: ShoppingCartProviderProps) => {
+  const [cartItems, setCartItems] = useState([])
+
+  const addToCart = (item: Product, amount: number) => {
+    // setCartItems(prev => {
+    //   if (prev.find(item => item.id === item.id) == null) {
+    //     return [...prev, {}]
+    //   }
+    // })
+  }
+  
+  return (
+    <ShoppingCartContext.Provider value={{addToCart, cartItems}}>
+      {children}
+    </ShoppingCartContext.Provider>
+  )
+}
